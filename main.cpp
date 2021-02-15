249b3a225042889b95b32f504f10c6f08b820bd9
krishpranav <krisna.pranav@gmail.com>
2021-02-15 16:25:15 +0530

    done changes

--- main.cpp
+++ main.cpp
@@ -52,24 +52,26 @@ string intToHexString(int intValue){
 
 	return hexStr
 }
-
 bool PromptForChar(const char* prompt, char& readch)
 {
-	std::string tmp;	
+	
+	std::string tmp;
 	std::cout << prompt << std::endl;
 	if (std::getline(std::cin, tmp))
 	{
+
 		if (tmp.length() == 1)
 		{
 			readch = tmp[0];
+
 		}
 		else
 		{
 			readch = '\0'
 		}
 		return true
-
 	}
+	
 	return false
 
 }
